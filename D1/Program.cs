4762856376b2923dbfc1430e7e6a584c825248e7
Llerak <llerak.exe@gmail.com>
2023-01-05 03:12:28 -0500

    Day 1 without optimizing

---
+++ D1/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Int32;
+
+string input = File.ReadAllText("../../../data/input.txt");
+
+string[] listElf = input.Split("\r\n\r\n");
+
+List<int> elfList = new List<int>();
+
+foreach (string var in listElf)
+{
+    if (var.Contains("\r\n"))
+    {
+        string[] comidas = var.Split("\r\n");
+        int total = 0;
+        foreach (string comida in comidas)
+        {
+            total += Parse(comida);
+        }
+        elfList.Add(total);
+    }
+    else
+    {
+        elfList.Add(Parse(var));
+    }
+    
+}
+
+int totalsuma = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    totalsuma += elfList.Max();
+    elfList.Remove(elfList.Max());
+    
+}
+
+Console.WriteLine(totalsuma);
